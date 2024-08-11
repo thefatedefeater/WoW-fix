7aacd877afcf7970617ee98c2710152498212389
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2024-08-11 14:14:00 +0330

    Update warpevil.py

--- warpevil.py
+++ warpevil.py
@@ -1,12 +1,15 @@
 import platform, subprocess, os, datetime, base64, json
-
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+import requests
 temp={
   "outbounds": 
   [
 
     {
     "type": "wireguard",
-    "tag": "Tel=@arshiacomplus Warp-IR1",
+    "tag": "",
+    "detour": "",
     "local_address": [
         "172.16.0.2/32",
         ""
@@ -22,28 +25,102 @@ temp={
     "fake_packets_size":"10-30",
     "fake_packets_delay":"10-30",
     "fake_packets_mode":"m4"
-    },
-    {
-    "type": "wireguard",
-    "tag": "Tel=@arshiacomplus Warp-Main1",
-    "detour": "Tel=@arshiacomplus Warp-IR1",
-    "local_address": [
-        "172.16.0.2/32",
-        ""
-    ],
-    "private_key": "",
-    "server": "",
-    "server_port": 0 ,
-    "peer_public_key": "",
-    "reserved": [],
-    "mtu": 1330,
-    "fake_packets_mode":"m4"
- 
     }
   ]
 }
     
+def byte_to_base64(myb):
+    return base64.b64encode(myb).decode('utf-8')
+     
 
+def generate_public_key(key_bytes):
+    # Convert the private key bytes to an X25519PrivateKey object
+    private_key = X25519PrivateKey.from_private_bytes(key_bytes)
+    
+    # Perform the scalar multiplication to get the public key
+    public_key = private_key.public_key()
+    
+    # Serialize the public key to bytes
+    public_key_bytes = public_key.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    )    
+    return public_key_bytes
+
+
+
+def generate_private_key():
+    key = os.urandom(32)    
+    # Modify random bytes using algorithm described at:
+    # https://cr.yp.to/ecdh.html.
+    key = list(key) # Convert bytes to list for mutable operations
+    key[0] &= 248
+    key[31] &= 127
+    key[31] |= 64    
+    return bytes(key) # Convert list back to bytes
+
+
+
+
+def register_key_on_CF(pub_key):
+    url = 'https://api.cloudflareclient.com/v0a4005/reg'
+    # url = 'https://api.cloudflareclient.com/v0a2158/reg'
+    # url = 'https://api.cloudflareclient.com/v0a3596/reg'
+
+    body = {"key": pub_key,
+            "install_id": "",
+            "fcm_token": "",
+            "warp_enabled": True,
+            "tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
+            "type": "Android",
+            "model": "PC",
+            "locale": "en_US"}
+
+    bodyString = json.dumps(body)
+
+    headers = {'Content-Type': 'application/json; charset=UTF-8',
+               'Host': 'api.cloudflareclient.com',
+               'Connection': 'Keep-Alive',
+               'Accept-Encoding': 'gzip',
+               'User-Agent': 'okhttp/3.12.1',
+               "CF-Client-Version": "a-6.30-3596"
+               }
+
+    r = requests.post(url, data=bodyString, headers=headers)
+    return r
+
+
+
+
+def bind_keys():
+    priv_bytes = generate_private_key()
+    priv_string = byte_to_base64(priv_bytes)
+    
+    
+    
+    
+    pub_bytes = generate_public_key(priv_bytes)
+    pub_string = byte_to_base64(pub_bytes)
+    
+    
+
+
+
+    result = register_key_on_CF(pub_string)
+    
+    if result.status_code == 200:
+        try:
+            z = json.loads(result.content)
+            client_id = z['config']["client_id"]      
+            cid_byte = base64.b64decode(client_id)
+            reserved = [int(j) for j in cid_byte]
+            
+            
+            return '2606:4700:110:846c:e510:bfa1:ea9f:5247/128',priv_string,reserved, 'bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo='
+            
+        except Exception as e:
+            print('Something went wronge with api')
+            exit()
 def arch_suffix():
     machine = platform.machine().lower()
     if machine.startswith('i386') or machine.startswith('i686'):
@@ -95,26 +172,22 @@ def export_Hiddify(t_ips, f_ips):
 
 def toSingBox(tag, clean_ip, detour):
     print("Generating Warp Conf")
-    config_url = "https://api.zeroteam.top/warp?format=warp-go"
-    conf_name = 'warp.conf'
-    subprocess.run(["wget", config_url, "-O", f"{conf_name}"])
-    cmd = ["./warp-go", f"--config={conf_name}", "--export-singbox=proxy.json"]
-    process = subprocess.run(cmd, capture_output=True, text=True)
-    output = process.stdout
 
-    if (process.returncode == 0) and output:
-        with open('proxy.json', 'r') as f:
-            data = json.load(f)
-            wg = data["outbounds"][0]
-            wg['server'] = clean_ip.split(':')[0]
-            wg['server_port'] = int(clean_ip.split(':')[1])
-            wg['mtu'] = 1300
-            wg['workers'] = 2
-            wg['detour'] = detour
-            wg['tag'] = tag
-        return wg
-    else:
-        return None
+
+    data = bind_keys()
+    wg = temp["outbounds"][0]
+    wg['private_key']=data[1]
+    wg['peer_public_key']=data[3]
+    wg['reserved']=data[2]
+    wg['local_address'][1]=data[0]
+    wg['server'] = clean_ip.split(':')[0]
+    wg['server_port'] = int(clean_ip.split(':')[1])
+    wg['mtu'] = 1300
+    wg['workers'] = 2
+    wg['detour'] = detour
+    wg['tag'] = tag
+    return wg
+
 
 
 def export_SingBox(t_ips, arch):
