4f5c33be5efacbd50d6da14865d621b29a18ee69
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2024-08-11 13:54:52 +0330

    Update warpevil.py

--- warpevil.py
+++ warpevil.py
@@ -1,37 +1,48 @@
 import platform, subprocess, os, datetime, base64, json
 
-temp=        {
-            "type": "wireguard",
-            "server": "162.159.195.161",
-            "server_port": 955,
-            "local_address": [
-                "172.16.0.2/32",
-                "2606:4700:110:8458:5c33:4e03:77d3:b5e3/128"
-            ],
-            "private_key": "",
-            "peer_public_key": "",
-            "reserved": [],
-            "mtu": 1300,
-            "workers": 2,
-            "detour": "direct",
-            "tag": "WARP-MAIN"
-        },
-        {
-            "type": "wireguard",
-            "server": "162.159.195.190",
-            "server_port": 987,
-            "local_address": [
-                "172.16.0.2/32",
-                "2606:4700:110:8a83:ecfd:99b4:8024:ef84/128"
-            ],
-            "private_key": "",
-            "peer_public_key": "",
-            "reserved": [],
-            "mtu": 1300,
-            "workers": 2,
-            "detour": "WARP-MAIN",
-            "tag": "WARP-WOW"
-        }
+temp={
+  "outbounds": 
+  [
+
+    {
+    "type": "wireguard",
+    "tag": "Tel=@arshiacomplus Warp-IR1",
+    "local_address": [
+        "172.16.0.2/32",
+        ""
+    ],
+    "private_key": "",
+    "peer_public_key": "",
+    "server": "",
+    "server_port": 0,
+    "reserved": [],
+
+    "mtu": 1280,
+    "fake_packets":"1-3",
+    "fake_packets_size":"10-30",
+    "fake_packets_delay":"10-30",
+    "fake_packets_mode":"m4"
+    },
+    {
+    "type": "wireguard",
+    "tag": "Tel=@arshiacomplus Warp-Main1",
+    "detour": "Tel=@arshiacomplus Warp-IR1",
+    "local_address": [
+        "172.16.0.2/32",
+        ""
+    ],
+    "private_key": "",
+    "server": "",
+    "server_port": 0 ,
+    "peer_public_key": "",
+    "reserved": [],
+    "mtu": 1330,
+    "fake_packets_mode":"m4"
+ 
+    }
+  ]
+}
+    
 
 def arch_suffix():
     machine = platform.machine().lower()
