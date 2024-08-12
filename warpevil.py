264da1ee60e67bb3084afc25f4f26132f8725649
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2024-08-12 14:35:56 -0700

    Update warpevil.py

--- warpevil.py
+++ warpevil.py
@@ -3,7 +3,7 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 import requests
 
-
+ 
 temp={
   "outbounds": 
   [
