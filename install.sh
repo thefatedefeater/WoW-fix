e555e7a86bc362b15e1577113f38fb5586483c57
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2024-08-12 13:21:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
-curl -fsSL -o ipv6.py https://raw.githubusercontent.com/arshiacomplus/WoW-fix/main/ipv6.py
-
+curl -fsSL -o WarpScanner.py https://raw.githubusercontent.com/arshiacomplus/WoW-fix/main/ipv6.py || { echo \"Failed to download ipv6.py. Exiting.\"; exit 1; }
 python ipv6.py
