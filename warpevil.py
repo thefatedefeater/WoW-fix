e73cbfa78a71d6ccd0d1ce3b5a8cbe4f17abfe43
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2024-08-14 06:18:28 -0700

    Update warpevil.py

--- warpevil.py
+++ warpevil.py
@@ -383,7 +383,7 @@ def main(script_dir):
 
     result_path = os.path.join(script_dir, 'result.csv')
     top_ips = export_bestIPS(result_path)
-    export_Hiddify(t_ips=top_ips, f_ips=result_path)
+
     export_SingBox(t_ips=top_ips, arch=arch)
     export_SingBox2(t_ips=top_ips, arch=arch)
     top_ips2 = export_bestIPS2("edge/assets/result.csv")
